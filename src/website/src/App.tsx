4da30d64020f1ea7a67164c1926ca904aa5d184a
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-03-07 00:58:51 +0000

    test react website, add beta header and vertical adds

--- src/website/src/App.tsx
+++ src/website/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="ads-container">
+                <div className="ad"><img src="amd.jpg"></img></div>
+                <div className="ad"><img src="amd.jpg"></img></div>
+            </div>
+        </div>
+    );
 }
 
+// <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//         Edit <code>src/App.tsx</code> and save to reload.
+//     </p>
+//     <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//     >
+//         Learn React
+//     </a>
+// </header>
 export default App;
+
