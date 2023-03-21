729f534a7a8dd3d3021d20d27d9b5ea900644b69
Diogo Bernardo <diogo.bernardo.dev@gmai..com>
2023-03-21 01:17:38 +0000

    sync locals

--- src/storeServer/settings.py
+++ src/storeServer/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
-#dev 
+# dev
 CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "storeServer.urls"
@@ -158,6 +158,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = "/var/www/tuxTech/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "website/build/static/"),
