9968893800ea11e4300e44dc1dfec82c08c16fdf
db <diogo.bernardo.dev@gmail.com>
2023-03-23 03:40:30 +0000

    add client views, serialization and url

--- src/users/views.py
+++ src/users/views.py
@@ -5,6 +5,10 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import JsonResponse
+from django.views import View
+from .models import Client
+from .serializers import ClientSerializer
 
 
 class LoginView(APIView):
@@ -27,6 +31,16 @@ class LoginView(APIView):
             return Response(
                 {"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
             )
+
+
+class ClientJsonView(View):
+    def get(self, request, *args, **kwargs):
+        try:
+            client = Client.objects.get(pk=kwargs["client_id"])
+            serializer = ClientSerializer(client)
+            return JsonResponse(serializer.data)
+        except Client.DoesNotExist:
+            return JsonResponse({"error": "Client not found"}, status=404)
 
 
 # Create your views here.
