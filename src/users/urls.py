617abab7610da3094a3f26b242fcba8f4e4c2510
db <diogo.bernardo.dev@gmail.com>
2023-03-23 03:40:30 +0000

    add client views, serialization and url

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import LoginView
+from .views import ClientJsonView
 
 urlpatterns = [
     path("api/login/", LoginView.as_view(), name="login"),
+    path("clients/<uuid:client_id>/", ClientJsonView.as_view(), name="client_json"),
 ]
