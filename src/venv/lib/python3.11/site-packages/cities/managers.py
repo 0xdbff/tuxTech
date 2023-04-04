ae52e85d2f7f713ab4b27c749f80fcd456547f56
db <diogo.bernardo.dev@gmail.com>
2023-04-04 12:52:33 +0100

    removed altered venv

--- src/venv/lib/python3.11/site-packages/cities/managers.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.gis.db import models
-
-
-class AlternativeNameManager(models.Manager):
-    def get_queryset(self):
-        return super(AlternativeNameManager, self).get_queryset().exclude(kind='link')
