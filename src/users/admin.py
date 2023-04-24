58d0fea0147383b59450cc21282d884c6de83fbb
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-04-24 21:39:22 +0100

    Fix user model!

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,13 +1,62 @@
 from django.contrib import admin
+from .models import TuxTechUser
 
-from .models import Client
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from .models import TuxTechUser, Client
+from django.utils.translation import gettext_lazy as _
 
-# from django.contrib.admin.models import LogEntry as DjangoLogEntry
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = TuxTechUser
+        fields = "__all__"
 
 
-# admin.site.register(CustomUser)
-admin.site.register(Client)
-# admin.site.register(Admin)
-# admin.site.register(DjangoLogEntry)
+class UserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    list_display = ("email", "username", "is_staff", "is_superuser")
+    search_fields = ("email", "username")
+    ordering = ("email",)
 
-# Register your models here.
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("username", "first_name", "last_name", "gender")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("user", "nif", "receive_news")
+    fields = ("user", "nif", "receive_news", "cart", "favourites")
+    search_fields = ("nif",)
+
+
+admin.site.register(TuxTechUser, UserAdmin)
+admin.site.register(Client, ClientAdmin)
