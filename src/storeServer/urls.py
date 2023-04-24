1ee9a759f50bbe786327ca027421ee2f9b5f8c4a
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-04-24 19:10:53 +0100

    remove 2fa

--- src/storeServer/urls.py
+++ src/storeServer/urls.py
@@ -17,13 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from two_factor.urls import urlpatterns as tf_urls
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("admin/", admin.site.urls),
-    path("2fa/", include(tf_urls)),
+    # path("2fa/", include(tf_urls)),
     path("users/", include("users.urls")),
     path("products/", include("product.urls")),
 ]
