d888d354daf4971863133660781c0aa349b5101a
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-04-29 05:03:58 +0100

    Fix website adds

--- src/website/src/App.tsx
+++ src/website/src/App.tsx
@@ -1,69 +1,15 @@
-// /* eslint-disable react-hooks/rules-of-hooks */
-//
-// import React, { useEffect } from "react";
-// import { useTheme } from "./themeContext";
-// import "./App.css";
-// import Registration from "./resgitration"; // !TODO CHANGE
-// import Images from "./images";
-//
-// const App: React.FC = () => {
-//     const { lightMode } = useTheme();
-//
-//     useEffect(() => {
-//         if (lightMode) {
-//             document.body.classList.add("dark-mode");
-//             document.body.classList.remove("light-mode");
-//         } else {
-//             document.body.classList.add("light-mode");
-//             document.body.classList.remove("dark-mode");
-//         }
-//     }, [lightMode]);
-//
-//     return (
-//         <div className="App">
-//             <div className="ad-left">
-//                 <img src="amd.jpg" alt=" " />
-//             </div>
-//             <div className="content">
-//                 <div
-//                     style={{
-//                         paddingTop: "80px",
-//                         textAlign: "center",
-//                         paddingBottom: "3000px",
-//                     }}
-//                 >
-//                     <Images></Images>
-//                     <h1>Website Content</h1>
-//                     <p>...</p>
-//                     <p>Contains bugs ATM, release expected 31/05!</p>
-//                     <Registration></Registration>
-//                 </div>
-//             </div>
-//             <div className="ad-right">
-//                 <img src="amd.jpg" alt=" " />
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default App;
-
-// App.tsx
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "./themeContext";
 import "./App.css";
 import Registration from "./resgitration";
 import Images from "./images";
 import ImageModal from "./ImageModal";
-import useBlurredBackground from "./useBlurredBackground";
-
 // const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const App: React.FC = () => {
     const { lightMode } = useTheme();
-    const blurredBackground = useBlurredBackground("path/to/your/image.jpg", 100);
 
-    const imageSrc = "./p047671_1_.jpg";
+    const imageSrc = "redhat2.png";
 
     useEffect(() => {
         if (lightMode) {
@@ -75,29 +21,72 @@ const App: React.FC = () => {
         }
     }, [lightMode]);
 
+    const applyBlurToImage = (
+        image: HTMLImageElement,
+        blurRadius: number,
+        containerWidth: number,
+        containerHeight: number
+    ): string => {
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        canvas.width = containerWidth;
+        canvas.height = containerHeight;
+
+        if (context) {
+            context.filter = `blur(${blurRadius}px)`;
+            context.drawImage(image, 0, 0, containerWidth, containerHeight);
+        }
+
+        return canvas.toDataURL();
+    };
+
     const handleContextMenu = (event: React.MouseEvent) => {
         event.preventDefault();
     };
 
+    const [blurredImage, setBlurredImage] = useState<string | null>(null);
+
+    useEffect(() => {
+        const image = new Image();
+        image.src = "redhat2.png";
+        image.onload = () => {
+            const banner = document.querySelector(".banner");
+            if (banner) {
+                const bannerRect = banner.getBoundingClientRect();
+                setBlurredImage(
+                    applyBlurToImage(image, 256, bannerRect.width, bannerRect.height)
+                );
+            }
+        };
+        image.onerror = () => {
+            setBlurredImage(null);
+        };
+    }, []);
     return (
         <div className="Main">
             <div className="left-banner">
                 <img src={`amd.jpg`} alt="Logo" onContextMenu={handleContextMenu} />
             </div>
+
             <div className="content">
-                <h1>Website Content</h1>
-                <p>...</p>
-                <p>Contains bugs ATM, release expected 31/05!</p>
-                <div
-                    className="banner"
-                    style={{
-                        backgroundImage: blurredBackground
-                            ? `url(${blurredBackground})`
-                            : "",
-                    }}
-                >
-                    <img src="redhat2.png" alt="Your ad image" />
+                <h1>Something here</h1>
+                <h1>Something here</h1>
+                <div className="banner">
+                    <div
+                        className="non-blurred-background"
+                        style={{
+                            backgroundImage: `url(${imageSrc})`,
+                        }}
+                    ></div>
+                    <div
+                        className="blurred-background"
+                        style={{
+                            backgroundImage: blurredImage ? `url(${blurredImage})` : "",
+                        }}
+                    ></div>
+                    <img src={imageSrc} alt="Your ad image" />
                 </div>
+
                 <Images></Images>
                 <Registration></Registration>
                 <Registration></Registration>
