86ee5706657de09d204bcb8e317ca9e092328722
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-04-30 18:41:19 +0100

    Clean project

--- src/website/src/index.tsx
+++ src/website/src/index.tsx
@@ -1,28 +1,18 @@
-// index.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
-import App from "./App";
-import { ThemeProvider } from "./contexts/themeContext";
 import reportWebVitals from "./reportWebVitals";
-import StoreHeader from "./components/Header";
-import Footer from "./components/Footer";
-
-const logoUrl = "https://example.com/logo.png";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <ThemeProvider>
-            <StoreHeader logoUrl={logoUrl} />
-            <App />
-            <Footer />
-        </ThemeProvider>
+        <App />
     </React.StrictMode>
 );
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
     reportWebVitals(console.log);
 }
