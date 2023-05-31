c21c32478c0b0d05bc68d545361fb56a4eb3dab0
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-05-31 20:17:06 +0100

    sync locals

--- src/website/src/pages/ProductInfo.tsx
+++ src/website/src/pages/ProductInfo.tsx
@@ -3,24 +3,28 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { getWebsiteUrl } from "../utils/path";
 import getAuthHeaders from "../utils/getAuthHeaders";
+import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import StoreHeader from "../components/Header";
 
-import { BaseInfo, DefaultVariant, Media } from "../components/BaseInfoDisplay"; // adjust this import according to your project structure
+import "../assets/css/productInfo.css";
+
+import { BaseInfo, DefaultVariant, Media } from "../components/BaseInfoDisplay";
 
 const ProductInfo: React.FC = () => {
+    const navigate = useNavigate();
     const { uuid } = useParams<{ uuid?: string }>();
 
     console.log(uuid);
     const [productInfo, setProductInfo] = useState<BaseInfo | null>(null);
 
     const fetchData = async (sku: string) => {
-        if (!sku) return; // do nothing if sku is undefined
+        if (!sku) return;
 
         try {
             const response = await axios.get(
-                `${getWebsiteUrl()}products/api/${sku}/`, // adjust this URL to match your API
-                { headers: getAuthHeaders() } // assuming getAuthHeaders is a function that returns necessary headers
+                `${getWebsiteUrl()}products/api/${sku}/`,
+                { headers: getAuthHeaders() }
             );
             console.log(response);
             setProductInfo(response.data);
@@ -37,36 +41,68 @@ const ProductInfo: React.FC = () => {
     if (!productInfo) {
         return <div></div>;
     }
-    // Check if the thumbnail and default variant exists before trying to use them
     const thumbnailUrl = productInfo.thumbnail?.image
         ? getWebsiteUrl() + productInfo.thumbnail.image
         : "";
     const price = productInfo.default_variant?.price;
 
-            const detailsArray = productInfo.details.split('.');
-        const detailsElements = detailsArray.map((detail, index) => {
-            const wordsArray = detail.split(' ');
-            const firstWord = wordsArray[0];
-            const restOfWords = wordsArray.slice(1).join(' ');
-            return (
-                <div key={index}>
-                    <h3>{firstWord}</h3>
-                    <p>{restOfWords}</p>
-                </div>
-            );
+    const processDetails = (details: string): React.ReactNode[] => {
+        const lines = details.split("\n").filter(Boolean);
+        return lines.map((line: string, index: number) => {
+            if (line === line.toUpperCase()) {
+                return <h3 key={index}>{line}</h3>;
+            } else {
+                return line.trim();
+            }
         });
+    };
 
-        return (
-            <div>
-                <div style={{ height: "50px" }}></div>
-                <h1>{productInfo.name}</h1>
+    return (
+        <div className="productInfoContainer">
+            <div className="productInfoContent">
+                <div className="productHeaderLinkBar">
+                    <a
+                        className="linkP"
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    >
+                        home
+                    </a>
+                    <a
+                        className="linkP"
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    >
+                        {" > " + productInfo.category}
+                    </a>
+                    <a
+                        className="linkP"
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    >
+                        {" > " + productInfo.subCategory}
+                    </a>
+                    <a
+                        className="linkP"
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    >
+                        {" > " + productInfo.ptype}
+                    </a>
+                </div>
+                <div className="productInfoHeader">Header</div>
+                <h2>{productInfo.name}</h2>
                 {thumbnailUrl && <img src={thumbnailUrl} alt={productInfo.name} />}
                 <p>{productInfo.description}</p>
                 {price && <p>Price: {price}€</p>}
-                {detailsElements}
+                {processDetails(productInfo.details)}
             </div>
-        );
-
+        </div>
+    );
 };
 
 const ProductInfoPage: React.FC = () => {
