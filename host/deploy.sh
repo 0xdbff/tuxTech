84e9dd1915cdfa1dd036324e907dff67cdae4c05
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-05-31 22:45:37 +0000

    add server files

--- host/deploy.sh
+++ host/deploy.sh
@@ -1,21 +1,31 @@
 #! /usr/bin/env bash
 
+# export DJANGO_SETTINGS_MODULE=storeServer.settings.prod
+
 sudo systemctl stop gunicorn.service
 sudo docker stop nginx-nginx-1
 
 cd /srv/nginx/tuxTech/src
+
+git pull origin main
 source ./venv/bin/activate
 
-./venv/bin/python3 ./manage.py makemigrations
-./venv/bin/python3 ./manage.py migrate
-./venv/bin/python3 ./manage.py collectstatic
-
-cd /srv/nginx/tuxTech/src/website/src/
-
+cd /srv/nginx/tuxTech/src/website
+source ~/.nvm/nvm.sh
+nvm use --lts
 npm install
 npm run build
 
+cd /srv/nginx/tuxTech/src
+# makemigrations
+# ./venv/bin/python3 ./manage.py collectstatic
+DJANGO_SETTINGS_MODULE=storeServer.settings.prod ./venv/bin/python3 ./manage.py collectstatic
+
 cd /srv/nginx/
 
-sudo docker start nginx-nginx-1
+sudo docker rm nginx-nginx-1
+sudo docker build  -t nginx-container .
+
+sudo docker compose up -d
+sudo systemctl daemon-reload
 sudo systemctl start gunicorn.service
