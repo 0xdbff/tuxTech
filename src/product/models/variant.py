c3a0e8979511e9636e37beb366f37de754174c64
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-05-31 03:07:54 +0100

    test change

--- src/product/models/variant.py
+++ src/product/models/variant.py
@@ -54,12 +54,18 @@ class Variant(models.Model):
     """ """
 
     def save(self, *args, **kwargs):
-        self._calculate_altered_specifications()
-
-        super().save(*args, **kwargs)
-
+        """
+        Overriding the save method to ensure only one variant is set as default for each product.
+        """
+        # If this variant is being set as the default,
+        # we set is_default to False for all other variants of this product.
         if self.is_default:
-            Variant.objects.set_default_variant(self)
+            Variant.objects.filter(info=self.info, is_default=True).exclude(
+                pk=self.pk
+            ).update(is_default=False)
+
+        # Now we can safely save.
+        super(Variant, self).save(*args, **kwargs)
 
     @property
     def stock(self):
